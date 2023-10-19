21a6eb2e3213fefbb0b537c85f0b34c4b34d673e
sikivpn <147810890+sikivpn@users.noreply.github.com>
2023-10-19 20:56:16 +0700

    Add files via upload

--- system/backup.sh
+++ system/backup.sh
@@ -1,70 +1,66 @@
-#wget https://github.com/${GitUser}/
-GitUser="sikivpn"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
-echo -e "\e[32mloading...\e[0m"
-clear
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
-echo -e "\e[32mloading...\e[0m"
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : sikivpn
+# (C) Copyright 2022
+# =========================================
+
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
 clear
 IP=$(wget -qO- icanhazip.com);
 date=$(date +"%Y-%m-%d")
 clear
-echo " Enter Your Email To Receive Message"
-read -rp " Email: " -e email
+echo " VPS Data Backup By sikivpn "
 sleep 1
-echo Directory Created
-mkdir /root/backup
-sleep 1
-echo Start Backup
+echo ""
+echo -e "[ ${green}INFO${NC} ] Please Insert Password To Secure Backup Data ."
+echo ""
+read -rp "Enter password : " -e InputPass
 clear
-cp /etc/passwd backup/
-cp /etc/group backup/
-cp /etc/shadow backup/
-cp /etc/gshadow backup/
-cp -r /etc/wireguard backup/wireguard
-cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-cp -r /var/lib/premium-script/ backup/premium-script
-cp -r /usr/local/etc/xray backup/xray
-cp -r /etc/trojan-go backup/trojan-go
-cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
-cp -r /home/vps/public_html backup/public_html
+sleep 1
+if [[ -z $InputPass ]]; then
+exit 0
+fi
+echo -e "[ ${green}INFO${NC} ] Processing . . . "
+mkdir -p /root/backup
+sleep 1
+clear
+echo " Please Wait VPS Data Backup In Progress . . . "
+#cp -r /root/.acme.sh /root/backup/ &> /dev/null
+#cp -r /var/lib/premium-script/ /root/backup/premium-script
+#cp -r /usr/local/etc/xray /root/backup/xray
+#cp -r /home/vps/public_html /root/backup/public_html
+cp -r /usr/local/etc/xray/ /root/backup/xray/ >/dev/null 2>&1
+cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
+cp -r /etc/crontab /root/backup/crontab &> /dev/null
+cp -r /etc/shadow /root/backup/shadow >/dev/null 2>&1
+cp -r /etc/gshadow /root/backup/gshadow >/dev/null 2>&1
+cp -r /etc/passwd /root/backup/passwd >/dev/null 2>&1
+cp -r /etc/group /root/backup/group >/dev/null 2>&1
 cd /root
-zip -r $IP-$date.zip backup > /dev/null 2>&1
+zip -rP $InputPass $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "The following is a link to your vps data backup file.
-
-Your VPS IP $IP
-
-$link
-
-If you want to restore data, please enter the link above.
-
-Thank You For Using Our Services" | mail -s "Backup Data" $email
+clear
+echo -e "\033[1;37mVPS Data Backup By sikivpn\033[0m
+\033[1;37mTelegram : https://t.me/sikivpn / @sikivpn\033[0m"
+echo ""
+echo "Please Copy Link Below & Save In Notepad"
+echo ""
+echo -e "Your VPS IP ( \033[1;37m$IP\033[0m )"
+echo ""
+echo -e "Your VPS Data Backup Password : \033[1;37m$InputPass\033[0m"
+echo ""
+echo -e "\033[1;37m$link\033[0m"
+echo ""
+echo "If you want to restore data, please enter the link above"
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
-echo "Done"
-echo "Please Check Your Email"
+echo ""
